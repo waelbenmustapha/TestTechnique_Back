962ca63e55ca8b10e341db10491a637cd811bb73
waelbenmustapha <[Your>
2021-08-21 12:34:02 +0100

    first push

---
+++ src/main/java/com/example/TestTechnique_Back/TestTechniqueBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.TestTechnique_Back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTechniqueBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestTechniqueBackApplication.class, args);
+	}
+
+}
